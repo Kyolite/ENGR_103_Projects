420028e2fbf30872b5963c6c1fd21e090001e1c0
Kyolite <davisky2@oregonstate.edu>
2023-10-01 22:55:40 -0700

    Asks the user for their favorite animal and then prints out their favorite animal. Latest version is finalized and runs without errors.

--- animal.py
+++
@@ -1,7 +0,0 @@
-# Author: Kyle Davis
-# Github username: Kyolite
-# Date: 10/01/2023
-# Description: Asks the user for their favorite animal and then prints out their favorite animal
-
-fave_animal = input("Please enter your favorite animal: ")
-print("Your favorite animal is the " + fave_animal + ".")
